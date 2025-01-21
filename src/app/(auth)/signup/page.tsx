bc3dcfaec753fb7e490979e85d8e448b406fad56
anees <anees@websquare.co.uk>
2025-01-21 11:56:47 +0500

    new change

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+import Signup from '@/src/components/signup/Signup'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <Signup/>
+    </div>
+  )
+}
--- src/components/signup/Signup.tsx
+++ src/components/signup/Signup.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function page() {
+export default function Signup() {
   return (
     <div>
-      <h1>Setting Page</h1>
+      <h1>Signup page</h1>
     </div>
   )
 }
